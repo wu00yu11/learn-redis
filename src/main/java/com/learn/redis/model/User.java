c05c6eeebc72e5cb176a18a686e7e6d90a2eb09a
wu00yu11 <wu00yu11@163.com>
2019-02-15 16:02:02 +0800

    可用实例代码提交

--- src/main/java/com/learn/redis/model/User.java
+++ src/main/java/com/learn/redis/model/User.java
@@ -12,6 +12,8 @@ import java.io.Serializable;
 @Data
 public class User implements Serializable {
 
+    private static final long serialVersionUID = 121959162708813585L;
+
     private Integer id;
     private String name;
     private String addr;
